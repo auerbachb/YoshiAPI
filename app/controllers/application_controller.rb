84e8c598582757c2b88f9f84a7723dc87bf5ad66
Haomin Wu <wuhaominsysu@gmail.com>
2017-11-03 23:47:53 -0400

    1. Refactor 2. Add error checking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,20 +6,8 @@ class ApplicationController < ActionController::API
                             query: { key: 'AIzaSyArSkeyF4aZEv0gaTV0KcYgIyCn2BwdHnM',
                                      placeid: place_id })
     JSON.parse(response.body)
-  end
-
-  # @param [JSON] response_json
-  # @return [Array] city, state, postcode
-  def get_other_info(response_json)
-    city = ''
-    state = ''
-    postcode = ''
-    response_json['result']['address_components'].each do |address_info|
-      city = address_info['long_name'] if address_info['types'].include? 'locality'
-      state = address_info['short_name'] if address_info['types'].include? 'administrative_area_level_1'
-      postcode = address_info['long_name'] if address_info['types'].include? 'postal_code'
-    end
-    [city, state, postcode]
+  rescue
+    {}
   end
 
   # @param [String] place_id
@@ -27,9 +15,17 @@ class ApplicationController < ActionController::API
   def get_info(place_id)
     response_json = get_json_response(place_id)
     street_name = response_json['result']['formatted_address']
-    other_info = get_other_info(response_json)
-    { streetAddress: street_name, city: other_info[0],
-      state: other_info[1], postalCode: other_info[2] }
+    city = '', state = '', postcode = ''
+    response_json['result']['address_components'].each do |address_info|
+      city = address_info['long_name'] if address_info['types'].include? 'locality'
+      state = address_info['short_name'] if address_info['types'].include? 'administrative_area_level_1'
+      postcode = address_info['long_name'] if address_info['types'].include? 'postal_code'
+    end
+    { streetAddress: street_name, city: city,
+      state: state, postalCode: postcode }
+  rescue
+    { streetAddress: '', city: '',
+      state: '', postalCode: '' }
   end
 
   # @param [String] lat latitude
@@ -42,6 +38,8 @@ class ApplicationController < ActionController::API
                                               rankby: 'distance' })
     current_place_json = JSON.parse(current_place_res.body)
     current_place_json['results'][0]['place_id']
+  rescue
+    ''
   end
 
   # @param [String] lat latitude
@@ -55,15 +53,26 @@ class ApplicationController < ActionController::API
                                             types: 'gas_station' })
     nearest_gas_json = JSON.parse(nearest_gas_res.body)
     nearest_gas_json['results'][0]['place_id']
+  rescue
+    ''
   end
 
   # @return [JSON] information of current location and nearest gas station
   def nearest_gas
+    if params[:lat].to_s.blank? || params[:lng].to_s.blank?
+      render json: { result: 'fail', error: 'Invalid Request' }
+      return
+    end
     lat = params[:lat]
     lng = params[:lng]
+    if lat.to_f < -90 || lat.to_f > 90 || lng.to_f < -180 || lng.to_f > 180
+      render json: { result: 'fail', error: 'Invalid coordinate' }
+      return
+    end
     current_place_info = get_info(get_current_place_id(lat, lng))
     nearest_gas_info = get_info(get_nearest_gs_place_id(lat, lng))
-    render json: { lat: lat, lng: lng,
-                   address: current_place_info, nearest_gas_station: nearest_gas_info }
+    render json: { result: 'suc', data: { address: current_place_info, nearest_gas_station: nearest_gas_info } }
+  rescue
+    render json: { result: 'fail', error: 'Internal Error' }
   end
 end
